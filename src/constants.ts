9d5e62f6cf876e6da0bab01c08a6ab49bfca76cf
andy lee <libin52008@163.com>
2023-10-31 10:29:24 +0800

    init

---
+++ src/constants.ts
@@ -0,0 +1,8 @@
+export const LISTS = {
+  "kurama": {
+    name: "Kurama",
+    keywords: ["kurama", "default"],
+    logoURI: "https://kurama-token.netlify.app/images/kurama/0x5c1fe01d69d2657455b6d4ae61500f2f42353e10.png",
+    sort: false,
+  },
+} as const;
